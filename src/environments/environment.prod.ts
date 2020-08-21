504b1ccd3209f0e747ec6811bf858de0656fadc6
mayank_singhal <techmayank2000@gmail.com>
2020-08-21 12:44:51 +0530

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyC8ECfpm2Ze93umMYxYpw2z3qXM0JGNo70",
+    authDomain: "todolistapp-ee048.firebaseapp.com",
+    databaseURL: "https://todolistapp-ee048.firebaseio.com",
+    projectId: "todolistapp-ee048",
+    storageBucket: "todolistapp-ee048.appspot.com",
+    messagingSenderId: "450271817676",
+    appId: "1:450271817676:web:5e995c8adcd1c04281f769",
+    measurementId: "G-MKBK3BC5SJ"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase : {
     apiKey: "AIzaSyC8ECfpm2Ze93umMYxYpw2z3qXM0JGNo70",
     authDomain: "todolistapp-ee048.firebaseapp.com",
